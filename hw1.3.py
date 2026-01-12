127da2c76921bef3f95ff849c07bbb0bdd35df12
js-303 <118151418+js-303@users.noreply.github.com>
2026-01-12 11:59:01 -0700

    Add files via upload
    removed plot of random points relative to curve, changed code from single N value to iterable for list of N values, added plot for estimations E(N) for N values.

--- hw1.3.py
+++ hw1.3.py
@@ -1,42 +1,52 @@
 import numpy as np
-from scipy import integrate 
+from scipy import integrate
+from math import floor
 import matplotlib.pyplot as plt
 
-def uniform_points(x_min,x_max,y_min,y_max, num_points):
-    x_cord = np.random.uniform(low=x_min,high=x_max,size=num_points)
-    y_cord = np.random.uniform(low=y_min,high=y_max,size=num_points)
+A = np.arange(1,5.1,0.1)
+M = []
+
+for i in range(len(A)):
+    M.append(floor(10**A[i]))
+
+def uniform_points(x_min,x_max,y_min,y_max, N):
+    x_cord = np.random.uniform(low=x_min,high=x_max,size=N)
+    y_cord = np.random.uniform(low=y_min,high=y_max,size=N)
     return np.vstack((x_cord,y_cord))
 
-points = uniform_points(0,1,0,1,5000)
-"""
-for i in range(len(points[0])):
-    x_cord = points[0][i]
-    g = lambda x_cord: (x_cord**4)/(1+x_cord**6)
-    if g <= points[1][i]:
-        ax.scatter(x_cord,g, color=r)
-"""
 xx = np.linspace(0,1,1000)
 f = lambda xx: (xx**4)/(1+xx**6)
-success = 0
-fail = 0
+
 fig, ax = plt.subplots(figsize=(8,6))
-for i in range(len(points[0])):
-    x_cord = points[0][i]
-    print("x cord: ",x_cord)
-    #g = lambda x_cord: (x_cord**4)/(1+x_cord**6)
-    if points[1][i] <= f(x_cord):
-        print("success! ", "g(x cord): ",f(x_cord), "y points: ", points[1][i])
-        ax.scatter(points[0][i],points[1][i], color='red')
-        success += 1
-    else:
-        print("fail! ","g(x cord): ",f(x_cord), "y points: ", points[1][i])
-        ax.scatter(points[0][i],points[1][i], color='blue')
-        fail += 1
-print("success: ", success, "fail: ", fail, "ratio: ", (success)/(fail))
+
+def estimate(points):
+    success = 0
+    fail = 0
+    for i in range(len(points[0])):
+        x_cord = points[0][i]
+        if points[1][i] < f(x_cord):
+            success += 1
+        else:
+            fail += 1
+    return(success/(fail+success))
+
+def E(N):
+    E = []
+    for n in range(len(N)):
+        points = uniform_points(0,1,0,1,N[n])
+        e = round(estimate(points),4)
+        E.append(e)
+    return E
 
 I, error = integrate.quad(f,0,1)
+I = round(I,4)
+error = round(error,4)
 print("I: ", I, "err: ",error)
-ax.plot(xx, f(xx), color='black')
-ax.set_xlim(0,1)
-ax.set_ylim(0,1)
+ax.set_title("Comparison of E(N) and I")
+ax.set_ylabel("E(N) estimate")
+ax.set_xlabel("N (log)")
+ax.set_xscale('log')
+ax.axhline(I, label='numerical solution to I')
+ax.scatter(M, E(M), label='estimate at N')
+ax.legend(loc='upper right')
 plt.show()
